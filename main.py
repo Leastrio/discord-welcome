b63a0911907a83429b83a0ee3ba766eff13b159a
Leastrio <60587129+Leastrio@users.noreply.github.com>
2020-10-07 13:26:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 from discord.ext import commands
 
+# Edit these!
 bot = commands.Bot(command_prefix = "!")
 channel_id = yourchannelid
 guild_id = yourguildid
@@ -15,6 +16,7 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
+    # Saves the Profile Picture as a file for PIL to edit it.
     with requests.get(member.avatar_url) as r:
         img_data = r.content
     with open('profile.jpg', 'wb') as handler:
@@ -22,9 +24,10 @@ async def on_member_join(member):
     im1 = Image.open("background.png")
     im2 = Image.open("profile.jpg")
 
+    # Font Stuff
     draw = ImageDraw.Draw(im1)
     font = ImageFont.truetype("BebasNeue-Regular.ttf", 32)
-
+    # Add the Text to the result image
     guild = bot.get_guild(guild_id)
     draw.text((160, 40),f"Welcome {member.name}",(255,255,255),font=font)
     draw.text((160, 80),f"You are the {guild.member_count}th member",(255,255,255),font=font)
@@ -32,20 +35,20 @@ async def on_member_join(member):
     size = 129
 
     im2 = im2.resize((size, size), resample=0)
-
+    # Creates the mask for the profile picture
     mask_im = Image.new("L", im2.size, 0)
     draw = ImageDraw.Draw(mask_im)
     draw.ellipse((0, 0, size, size), fill=255)
 
     mask_im.save('mask_circle.png', quality=95)
 
-
+    # Masks the profile picture and adds it to the background.
     back_im = im1.copy()
     back_im.paste(im2, (11, 11), mask_im)
 
 
     back_im.save('welcomeimage.png', quality=95)
-
+    # Stuff to send the embed with a local image.
     f = discord.File(path, filename="welcomeimage.png")
 
     embed = discord.Embed()
